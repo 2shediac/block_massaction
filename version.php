546bb48179e8170e04b99cfcca3699fa0388a70c
Derek Henderson <derek.henderson@remote-learner.net>
2016-04-20 14:59:59 -0400

    Second try

--- version.php
+++ version.php
@@ -3,4 +3,4 @@ $plugin->component = 'block_massaction'; // Required in Moodle 3.0.
 $plugin->version = 2016030400;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2012110900; // YYYYMMDDHH (This is the release version for Moodle 2.0).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.2.2';
+$plugin->release = 'v1.2.3';
